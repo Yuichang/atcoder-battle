5fe02c9c49815146a41f0bfc782c5175856a3393
Yuichang <zuka0726@gmail.com>
2026-04-05 01:13:04 +0900

    add gin

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/Yuichang/atcoder-battle/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// 静的ファイル
+	r.Static("/static", "./static")
+
+	// テンプレート読み込み
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", handlers.ShowIndex)
+
+	// サーバ起動(http://localhost:8080)
+	r.Run()
+}
